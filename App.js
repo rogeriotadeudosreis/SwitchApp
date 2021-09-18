9a524c954c5da592ab6c9d0dabbadcbfa7e3c829
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-09-18 19:12:22 -0300

    estilizacao do switch

--- App.js
+++ App.js
@@ -3,22 +3,25 @@ import {Switch, Text, View} from 'react-native';
 import styles from './src/assets/styles';
 
 export default function App() {
-  const [habilitado, setHabilitado] = useState(false);
+  const [isEnable, setIsEnable] = useState(false);
 
-  function valorDoSwitch() {
-    setHabilitado(estadoAnterior => !estadoAnterior);
-    console.warn(habilitado);
+  function alterarSwitch() {
+    setIsEnable(previousState => !previousState);
+    console.warn(isEnable);
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Aula de Switch</Text>
       <Switch
-        trackColor={{false: '#767577', true: '#f4f3f4'}}
-        thumbColor={habilitado ? 'blue' : '#f4f3f4'}
-        onValueChange={valorDoSwitch}
-        value={habilitado}
+        trackColor={{false: '#95a5a6', true: '#95a5a6'}}
+        thumbColor={isEnable ? 'blue' : '#2c3e50'}
+        onValueChange={alterarSwitch}
+        value={isEnable}
       />
+      <View>
+        <Text style={styles.texto}>{isEnable ? 'Switch Ligado' : 'Switch Desligado'}</Text>
+      </View>
     </View>
   );
 }
